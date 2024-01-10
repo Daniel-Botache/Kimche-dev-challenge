8641ab31131a120372ff52d73b42b0af2666d5ca
Daniel-Botache <daniel.botache@gmail.com>
2024-01-10 18:51:51 -0500

    se hacen estilos de la navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import FilterBar from "../FilterBar/FilterBar";
 import SearchBar from "../SearchBar/SearchBar";
+import style from "./NavBar.module.css";
 
 export default function NavBar({
   onSearch,
@@ -18,23 +19,45 @@ export default function NavBar({
   };
 
   return (
-    <div>
-      <SearchBar onSearch={onSearch} />
-      <div>
+    <div className={style.principalContainer}>
+      <div className={style.searchCointainer}>
+        <div className={style.tittleContainer}>
+          <div className={style.title}>
+            Rick
+            <span className={style.spanTitle}>and</span>
+            Morty
+            <span className={style.spanTitle}>APP</span>
+          </div>
+          <div className={`${style.title} ${style.middle}`}>
+            Rick
+            <span className={style.spanTitle}>and</span>
+            Morty
+            <span className={style.spanTitle}>APP</span>
+          </div>
+          <div className={`${style.title} ${style.bottom}`}>
+            Rick
+            <span className={style.spanTitle}>and</span>
+            Morty
+            <span className={style.spanTitle}>APP</span>
+          </div>
+        </div>
+        <SearchBar onSearch={onSearch} />
         <button onClick={toggleFilterBar}>
           {isFilterBarVisible ? "Ocultar Filtros" : "Mostrar Filtros"}
         </button>
       </div>
-      {isFilterBarVisible && (
-        <FilterBar
-          statuses={statuses}
-          genders={genders}
-          species={species}
-          onStatusChange={onStatusChange}
-          onGenderChange={onGenderChange}
-          onSpecieChange={onSpecieChange}
-        />
-      )}
+      <div className={style.filterContainer}>
+        {isFilterBarVisible && (
+          <FilterBar
+            statuses={statuses}
+            genders={genders}
+            species={species}
+            onStatusChange={onStatusChange}
+            onGenderChange={onGenderChange}
+            onSpecieChange={onSpecieChange}
+          />
+        )}
+      </div>
     </div>
   );
 }
