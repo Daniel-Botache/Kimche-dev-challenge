fa33ceab69ad3cce3c191ac819f92ca72f2241f5
Daniel-Botache <daniel.botache@gmail.com>
2024-01-10 09:13:59 -0500

    se completa el paginador

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Card from "../Card/Card";
 
-export default function Cards({ data }) {
-  const characters = data.characters.results;
-  console.log();
+export default function Cards({ characters }) {
+  /* const characters = allCharacters.characters.results; */
+
+  console.log(characters);
   return (
     <div>
       {characters.map(({ name, image }) => (
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,37 +1,86 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Cards from "../components/Cards/Cards";
 import NavBar from "../components/NavBar/NavBar";
 import { gql, useQuery } from "@apollo/client";
 
 export default function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [allCharacters, setAllCharacters] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const charactersPerPage = 20; // Cantidad de personajes por página
+
   const ALL_CHARACTERS = gql`
-    query {
-      characters {
-        results {
-          name
-          image
-        }
+  query {
+    characters(page: ${currentPage}) {
+      info {
+        count,
+        pages
+      }
+      results {
+        name,
+        image
       }
     }
+  }
   `;
-
-  const { error, loading, data } = useQuery(ALL_CHARACTERS);
+  console.log(allCharacters);
+  const { loading, data } = useQuery(ALL_CHARACTERS, {
+    variables: { page: currentPage, perPage: charactersPerPage },
+  });
 
   useEffect(() => {
-    // Puedes manejar los resultados, errores, etc. aquí
     if (data) {
-      console.log(data);
-      // Puedes establecer el estado de tus personajes aquí
+      // Concatena los nuevos resultados con los existentes
+      const totalPages = data.characters.info.pages;
+      setTotalPages(totalPages);
+      console.log(totalPages);
+      const characters = data.characters.results;
+      setAllCharacters(characters);
     }
-    if (error) {
-      console.error(error);
-    }
-  }, [data, error]);
+  }, [data]);
+
+  const loadMore = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <div>
       <NavBar />
-      <Cards data={data} />
+      <Cards characters={allCharacters} />
+      {totalPages === 1 ? (
+        <></>
+      ) : (
+        <div>
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            ˂ Prev
+          </button>
+          <span>
+            <span
+              style={{ fontWeight: "bold", fontFamily: "'Inter', sans-serif'" }}
+            >
+              {currentPage}
+            </span>
+            <span> to </span>
+            <span
+              style={{ fontWeight: "bold", fontFamily: "'Inter', sans-serif'" }}
+            >
+              {totalPages}
+            </span>
+          </span>
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next ˃
+          </button>
+        </div>
+      )}
     </div>
   );
 }
